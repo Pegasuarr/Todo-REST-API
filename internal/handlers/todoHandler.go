2ef504f7670f63ebb6c6343a9b2b156065064d00
Pegasuarr <heng525555@gmail.com>
2026-06-18 20:55:56 +0700

    creating todo handler

---
+++ internal/handlers/todoHandler.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"Todo_App/internal/repository"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type CreateTodoInput struct {
+	Title     string `json:"title" binding:"required"`
+	Completed bool   `json:"completed"`
+}
+
+func CreateTodoHandler(pool *pgxpool.Pool) gin.HandlerFunc{
+	return func(c *gin.Context) {
+		var input CreateTodoInput
+
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		todo, err := repository.CreateTodo(pool, input.Title, input.Completed)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, todo)
+	}
+}
