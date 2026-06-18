a1254407b9f431eb78f8eaeffaf6f5032e8626a7
Pegasuarr <heng525555@gmail.com>
2026-06-18 20:24:06 +0700

    creating Todo repository

---
+++ internal/repository/todoRepository.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"Todo_App/internal/models"
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+func CreateTodo(pool *pgxpool.Pool, title string, completed bool) (*models.Todo, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := `INSERT INTO todos (title, completed)
+		VALUES ($1, $2)
+		RETURNING id, title, completed, created_at, updated_at`
+
+	var todo models.Todo
+	err := pool.QueryRow(ctx, query, title, completed).Scan(
+		&todo.ID,
+		&todo.Title,
+		&todo.Completed,
+		&todo.CreatedAt,
+		&todo.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &todo, nil
+}
